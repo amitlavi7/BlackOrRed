79a9c12f76577dfc281da9de387294f7a08ed450
amitlavi7 <amitlavi7@gmail.com>
2020-09-12 16:56:48 +0300

    Added alert for existing player name

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import alert from 'alert';
+const alert = require('alert');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -29,44 +31,47 @@ app.post('/', (req, res) => {
     //move to player page
     response.sendFile(__dirname + '/player.html');
     app.post(groupURL, (req, res) => {
-      let playerName = req.body.playerName;
-      let playerURL = groupURL + "/" + playerName;
-      // var card = "";
-      players.push(playerName);
-      createGame(numOfPlayers);
-      res.redirect(playerURL);
+      let playerName = req.body.playerName.toLowerCase();
+      if (players.includes(playerName)) {
+        alert('Player name is already exists, please choose another name');
+      } else {
+        let playerURL = groupURL + "/" + playerName;
+        // var card = "";
+        players.push(playerName);
+        createGame(numOfPlayers);
+        res.redirect(playerURL);
 
-      app.get(playerURL, (req, res) => {
-        res.write("<h1>" + playerName + "</h1>");
-        if (reds.includes(playerName)){
-          chooseCard("red");
-          res.write("<h3>you are red!</h3>");
-          // card = "red";
-          res.write('<img class="card" src="https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/933/KH__01216.1440113580.1280.1280.png?c=2" alt="You Are Red!">');
-          // res.sendFile(__dirname + "/redCard.html");
-        }
-        else if (blacks.includes(playerName)){
-          chooseCard("black");
-          res.write("<h3>you are black!</h3>");
-          // card = "Red";
-          res.write('<img class="card" src="https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/935/AS__68652.1440113599.1280.1280.png?c=2" alt="You Are Black!">');
-          // res.sendFile(__dirname + "/blackCard.html");
-        }
-        // response.sendFile(__dirname + '/card.html');
-        // res.render('card', {card: card})
-        // res.write("red is: " + reds + ", blacks are: " + blacks);
-        // res.write(", players are: " + players);
+        app.get(playerURL, (req, res) => {
+          res.write("<h1>" + playerName + "</h1>");
+          if (reds.includes(playerName)) {
+            chooseCard("red");
+            res.write("<h3>you are red!</h3>");
+            // card = "red";
+            res.write('<img class="card" src="https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/933/KH__01216.1440113580.1280.1280.png?c=2" alt="You Are Red!">');
+            // res.sendFile(__dirname + "/redCard.html");
+          } else if (blacks.includes(playerName)) {
+            chooseCard("black");
+            res.write("<h3>you are black!</h3>");
+            // card = "Red";
+            res.write('<img class="card" src="https://cdn2.bigcommerce.com/n-d57o0b/1kujmu/products/297/images/935/AS__68652.1440113599.1280.1280.png?c=2" alt="You Are Black!">');
+            // res.sendFile(__dirname + "/blackCard.html");
+          }
+          // response.sendFile(__dirname + '/card.html');
+          // res.render('card', {card: card})
+          // res.write("red is: " + reds + ", blacks are: " + blacks);
+          // res.write(", players are: " + players);
 
-        if(numOfPlayers === players.length)
-          res.write("<h2>All players has been logged in, please refresh to start</h2>");
-        // while(numOfPlayers !== players.length){
-        //   setTimeout(function () {
-        //     res.redirect('back');
-        //   }, 5000);
-        // }
+          if (numOfPlayers === players.length)
+            res.write("<h2>All players has been logged in, please refresh to start</h2>");
+          // while(numOfPlayers !== players.length){
+          //   setTimeout(function () {
+          //     res.redirect('back');
+          //   }, 5000);
+          // }
 
-        res.send();
-      });
+          res.send();
+        });
+      }
     });
   });
 });
@@ -74,11 +79,11 @@ app.post('/', (req, res) => {
 const createGame = (numOfPlayers) => {
   blacks = [];
   reds = [];
-  if(numOfPlayers === players.length){
+  if (numOfPlayers === players.length) {
     let numOfReds = 0;
-    if(numOfPlayers < 6)
+    if (numOfPlayers < 6)
       numOfReds = 1;
-    else if(numOfPlayers < 8)
+    else if (numOfPlayers < 8)
       numOfReds = 2;
     else
       numOfReds = 3;
@@ -88,7 +93,7 @@ const createGame = (numOfPlayers) => {
       while (numOfReds > 0) {
         redPlayerIndex = Math.floor(Math.random() * players.length);
         let redPlayer = players[redPlayerIndex];
-        if (!reds.includes(redPlayer)){
+        if (!reds.includes(redPlayer)) {
           reds.push(redPlayer);
           numOfReds--;
         }
@@ -124,17 +129,12 @@ const chooseCard = (color) => {
   let chooseShape = () => Math.floor(Math.random() * 2);
   let chooseNumber = () => Math.floor(Math.random() * 14);
 
-  if(color === "black"){
+  if (color === "black") {
     card = "blacks/" + blacks[chooseShape()] + "_" + numbers[chooseNumber()];
   } else {
     card = "reds/" + reds[chooseShape()] + "_" + numbers[chooseNumber()];
   }
 }
-
-// function updateTextInput(val) {
-//   document.getElementById('textInput').value = val;
-// }
-
 
 app.listen(process.env.PORT || 3030, () => console.log("server is up"));
 
